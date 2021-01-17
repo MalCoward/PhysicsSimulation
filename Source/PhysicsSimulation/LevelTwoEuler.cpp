78169ae45aa4523441d344a97ae7f0c5af7c201b
Malachi Coward <60357765+MalCoward@users.noreply.github.com>
2021-01-17 17:04:40 +0000

    Starting Part 2

---
+++ Source/PhysicsSimulation/LevelTwoEuler.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LevelTwoEuler.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values
+ALevelTwoEuler::ALevelTwoEuler()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	currentVelocity = { 0.0f / 60, 0.0f, 0.0f / 60 };
+	gravityAcc = { 0.0f, 0.0f, 0.0f / 60 };
+
+}
+
+// Called when the game starts or when spawned
+void ALevelTwoEuler::BeginPlay()
+{
+	Super::BeginPlay();
+
+	currentPosition = this->GetActorLocation();
+	startFrame = 0;
+	currentFrame = startFrame;
+	newPosition = currentPosition;
+	newVelocity = currentVelocity;
+
+}
+
+// Called every frame
+void ALevelTwoEuler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (this->GetActorLocation().Z <= 0)
+	{
+		return;
+	}
+	else
+	{
+		currentFrame++;
+		UpdatePosition();
+	}
+}
+
+void ALevelTwoEuler::UpdatePosition()
+{
+
+	newVelocity = currentVelocity + gravityAcc;
+	newPosition = currentPosition + newVelocity;
+
+	currentVelocity = newVelocity;
+	currentPosition = newPosition;
+
+	this->SetActorLocation(currentPosition);
+
+}
---
+++ Source/PhysicsSimulation/LevelTwoEuler.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LevelTwoEuler.generated.h"
+
+UCLASS()
+class PHYSICSSIMULATION_API ALevelTwoEuler : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelTwoEuler();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Position)
+		FVector currentPosition;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Position)
+		FVector newPosition;
+
+	FVector currentVelocity;
+	FVector newVelocity;
+
+	FVector gravityAcc;
+
+	float startFrame;
+	float currentFrame;
+
+	void UpdatePosition();
+
+};
